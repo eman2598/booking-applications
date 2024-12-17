009fb553ddcbb679e6f2721738c992211d6e092f
eman2598 <zuni259876>
2024-12-17 20:03:40 +0500

    compile css, js and image files

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,34 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+
+                // auth theme css files
+                'resources/assets/vendors/mdi/css/materialdesignicons.min.css',
+                'resources/assets/vendors/css/vendor.bundle.base.css',
+                'resources/assets/css/style.css',
+                'resources/assets/images/favicon.png',
+                // auth theme js files
+                'resources/assets/vendors/js/vendor.bundle.base.js',
+                'resources/assets/js/off-canvas.js',
+                'resources/assets/js/hoverable-collapse.js',
+                'resources/assets/js/misc.js',
+                'resources/assets/js/settings.js',
+                'resources/assets/js/todolist.js',
+
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
+
+    build: {
+        sourcemap: false, // Disable source map generation
+        rollupOptions: {
+            output: {
+                sourcemap: false, // Prevent Rollup from generating or looking for source maps
+            },
+        },
+    },
 });
